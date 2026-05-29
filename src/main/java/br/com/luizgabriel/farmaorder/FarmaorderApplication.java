1c4497c554ad6a358c748ce1020f5117e334b35b
Luiz Gabriel <luiz.gabrielcb2006@gmail.com>
2026-05-29 12:30:23 -0300

    chore: initial project setup

---
+++ src/main/java/br/com/luizgabriel/farmaorder/FarmaorderApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizgabriel.farmaorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luizgabriel/farmaorder/FarmaorderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizgabriel.farmaorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
