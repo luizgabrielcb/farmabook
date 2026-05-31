8b959868d5edd358822366b1895fc74fe437a3f9
Luiz Gabriel <luiz.gabrielcb2006@gmail.com>
2026-05-31 14:34:43 -0300

    feat: add shortage controller CRUD with PIN-by-action

--- src/main/java/br/com/luizgabriel/farmaorder/stock/shortage/ShortageMapper.java
+++ src/main/java/br/com/luizgabriel/farmaorder/stock/shortage/ShortageMapper.java
@@ -11,9 +11,13 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface ShortageMapper {
 
+    @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdById", source = "createdBy.id")
     @Mapping(target = "createdByName", source = "createdBy.name")
     @Mapping(target = "status", constant = "PENDING")
+    @Mapping(target = "orderedById", ignore = true)
+    @Mapping(target = "orderedByName", ignore = true)
+    @Mapping(target = "orderedAt", ignore = true)
     Shortage toShortage(ShortagePostRequest request, User createdBy);
 
     ShortagePostResponse toShortagePostResponse(Shortage shortage);
